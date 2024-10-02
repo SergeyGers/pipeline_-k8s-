e56573adc597e5d6ebf985bd39f51a95a156e72d
sergey.gershov <gers7777@gmail.com>
2024-10-02 10:42:36 +0300

    terraform file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,5 @@
+# outputs.tf
+
 output "eks_cluster_name" {
   description = "The name of the EKS cluster"
   value       = module.eks.cluster_id
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,57 @@
-variable "cluster_name" {
-  default = "my-eks-cluster"
+# variables.tf
+
+variable "name" {
+  description = "Name of the project and EKS cluster"
+  type        = string
+  default     = "my_project"
 }
 
 variable "region" {
-  default = "us-east-1"
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+variable "cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.10.0.0/16"
 }
 
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.10.1.0/24", "10.10.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.10.3.0/24", "10.10.4.0/24"]
+}
+
+variable "intra_subnets" {
+  description = "Intra subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.10.5.0/24", "10.10.6.0/24"]
+}
+
+variable "instance_types" {
+  description = "EC2 instance types for the EKS node group"
+  type        = list(string)
+  default     = ["t3.micro", "t3.small", "t3.medium"]
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {
+    Example = "my_project"
+  }
+}
